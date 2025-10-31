7567c5f5cac1779cbef08f61f04f0bb1913733ff
yurihoka <yurihoka@gmail.com>
2025-10-31 18:32:22 +0900

    create dashboard

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./dashboard.css";
+import search_icon from "../../assets/search-icon.png";
+import add_icon from "../../assets/add-icon.png";
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <div className="dashboard-top">
+        <h1>My Notes</h1>
+        <img src={search_icon} alt="" />
+      </div>
+      <div className="dashboard-bottom">
+        <img src={add_icon} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
